b9a8fd5479239cc5c776b7065633632403c1ecf2
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-10-16 12:46:35 +0900

    IDLEで実行した処理のテキストファイルを追加しました

---
+++ 1016/IDLEでの実行処理.py
@@ -0,0 +1,80 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 10/3
+3.3333333333333335
+>>> 10//3
+3
+>>> 10/5
+2.0
+>>> 10//5
+2
+>>> 2**8
+256
+>>> 2**2
+4
+>>> 2**3
+8
+>>> 2**16
+65536
+>>> 2**500
+3273390607896141870013189696827599152216642046043064789483291368096133796404674554883270092325904157150886684127560071009217256545885393053328527589376
+>>> a=10
+>>> a
+10
+>>> aa
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    aa
+NameError: name 'aa' is not defined. Did you mean: 'a'?
+>>> a
+10
+>>> type(a)
+<class 'int'>
+>>> s="aaa"
+>>> type(a)
+<class 'int'>
+>>> type(s)
+<class 'str'>
+>>> f = 10/5
+type(f)
+<class 'float'>
+1-/5
+SyntaxError: invalid syntax
+10/5
+2.0
+10/3
+3.3333333333333335
+10//3
+3
+e = 10//3
+type(e)
+<class 'int'>
+type(10)
+<class 'int'>
+3 == 3
+True
+type 3==3
+SyntaxError: invalid syntax
+type 3 == 3
+SyntaxError: invalid syntax
+type(3==3)
+<class 'bool'>
+a=3
+b=3
+a==b
+True
+c=4
+a==c
+False
+d = True
+d
+True
+type
+<class 'type'>
+type(d)
+<class 'bool'>
+type(true)
+Traceback (most recent call last):
+  File "<pyshell#39>", line 1, in <module>
+    type(true)
+NameError: name 'true' is not defined. Did you mean: 'True'?
