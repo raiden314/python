ab890a04aae04d99875f794bebdd57be5b683747
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-10-16 12:10:05 +0900

    前回の処理をフォーマット使って出力

--- 1016/test05.py
+++ 1016/test05.py
@@ -4,4 +4,6 @@ year = input("生まれた年は（西暦）は？")
 # 演算するために変数yearを整数型に型変換
 age = 2023 - int( year )
 # 文字列結合するために変数ageを文字列型に型変換
-print( "2023年あなたは" + str( age ) + "歳です" )
+# print( "2023年あなたは" + str( age ) + "歳です" )
+str = "2023年あなたは{0}歳です"
+print( str.format(age) )
