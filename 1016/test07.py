45fd618dd8e840e23ec96bf8c1bc8e804180b114
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-10-16 12:43:27 +0900

    前回の処理にさらに分岐を追加しました

--- 1016/test07.py
+++ 1016/test07.py
@@ -2,8 +2,11 @@ a = 100
 b = 10
 # bool型は"True" or "False"　大文字で始まる
 if a == b :
-    print("同じ",end="")
+    print("同じ")
+
+elif a > b :
+    print("aが大きい")
 else :
-    print("違う",end="")
+    print("違う")
 
 print("値")
