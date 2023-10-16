76dc3433c51f17c8931f40935c1f76cbb9914516
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-10-16 11:58:26 +0900

    2023年時点での年齢を出力する処理

--- 1016/test05.py
+++ 1016/test05.py
@@ -1,2 +1,7 @@
-age = input("生まれた年は（西暦）は？")
-print( age )
+# age = input("生まれた年は（西暦）は？")
+# print( age )
+year = input("生まれた年は（西暦）は？")
+# 演算するために変数yearを整数型に型変換
+age = 2023 - int( year )
+# 文字列結合するために変数ageを文字列型に型変換
+print( "2023年あなたは" + str( age ) + "歳です" )
