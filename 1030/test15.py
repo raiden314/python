d998ac8794f268d0ac3c14ce233275c82184f463
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-10-30 10:25:01 +0900

    二つのリストを結合

--- 1030/test15.py
+++ 1030/test15.py
@@ -1,8 +1,12 @@
 slst = ["名古屋","金山","神宮前"]
+addlst = ["新安城","東岡崎","豊橋"]
 
 # 要素を追加
 slst.append("知立")
 
+# 二つのリストを結合
+lst = slst + addlst
+
 # 93ページをチェック
-for i, val in enumerate(slst):
+for i, val in enumerate(lst):
     print(i, val)
