e17d0c080337764a2e78cbb176eabfae4bee17f8
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-11-13 11:48:27 +0900

    仲介手数料を計算する関数を作りました

---
+++ 1113/calcFunc.py
@@ -0,0 +1,14 @@
+def feeCalc( amt ) :
+    fee = 0
+    if ( amt <= 200 ) :
+        fee = amt * 0.05
+    elif ( amt > 400 ) :
+        fee = amt * 0.03 + 6
+    else :
+        fee = amt * 0.04 + 2
+    return fee * 1.1
+
+
+amount = input("売買価格を入力してください")
+fee = feeCalc(float(amount))
+print("仲介手数料は", fee, "万円です")
