14def7ae25a870b60d707378f8bad039374caf3c
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-11-13 12:51:30 +0900

    filter関数を使って、任意の要素だけを取り出す。filter関数は、trueと返ってきたものだけを取り出す。

--- 1113/test20.py
+++ 1113/test20.py
@@ -3,3 +3,8 @@ x2 = lambda x : x * 2
 num = [1,3,5,7,9]
 lst = list( map(x2,num) )
 print(lst)
+
+x3 = lambda x : (x%2)==0
+nums = [1,2,3,11,12,13,21,22,23]
+lst = list( filter( x3,nums ) )
+print(lst)
