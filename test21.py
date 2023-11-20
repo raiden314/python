63aa87c7fb3488b873897ca043344859c883b30d
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-11-20 10:35:28 +0900

    値を元にして、昇順に並び替え

--- test21.py
+++ test21.py
@@ -4,6 +4,10 @@ animal_list = [
     ("シマウマ", 60),
     ("トナカイ", 80),
 ]
-fasted_list = sorted(animal_list)
+fasted_list = sorted(
+    animal_list,
+                        #listの一番始めを取る
+    key = lambda  ani : ani[1]
+    )
 
 for i in fasted_list : print(i)
