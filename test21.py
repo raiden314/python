49e0fb9bdc6809ee9cbb718b5c06fcfa5d38543a
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-11-20 10:35:56 +0900

    値を元にして、降順に並び替え

--- test21.py
+++ test21.py
@@ -7,7 +7,8 @@ animal_list = [
 fasted_list = sorted(
     animal_list,
                         #listの一番始めを取る
-    key = lambda  ani : ani[1]
+    key = lambda  ani : ani[1],
+    reverse = True
     )
 
 for i in fasted_list : print(i)
