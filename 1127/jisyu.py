bc5aef639b29b88890af1a8415b93e31f114364d
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-11-27 11:34:06 +0900

    問２を別のパッケージをインストールして実装しました。

--- 1127/jisyu.py
+++ 1127/jisyu.py
@@ -1,6 +1,26 @@
 import calcFunc
 
+from cryptography.fernet import Fernet
+import base64
+
 #1
 amount = input("売買価格を入力してください")
 fee = calcFunc.feeCalc(float(amount))
 print("仲介手数料は" +  str(fee) + "万円です")
+
+
+#2
+# 暗号化・復号化対象のメッセージ
+data = "日本語で書かれた秘密のメッセージ".encode("utf-8")
+
+# インスタンス生成
+key = Fernet.generate_key()
+f = Fernet(key)
+
+# 暗号化されたデータ
+token = f.encrypt(data)
+print(token)
+
+# 復号化されたデータ
+data_decrypt = f.decrypt(token)
+print(f.decrypt(token).decode("utf-8"))
