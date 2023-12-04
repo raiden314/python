48f941e0b722f405ba2f36869644c0468a77e0ed
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-12-04 12:46:45 +0900

    複数の画像ファイルをダウンロードする処理を実装しました。

--- 1204/imgdownload.py
+++ 1204/imgdownload.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib
 
 url = "https://www.ymori.com/books/python2nen/test2.html"
 html = requests.get(url)
@@ -9,4 +10,9 @@ soup = BeautifulSoup(html.content, "html.parser")
 imgele = soup.find_all("img")
 for ele in imgele:
     src = ele.get("src")
-    print(src)
+    imgurl = urllib.parse.urljoin(url,src)
+    print(imgurl)
+    imgdata = requests.get(imgurl)
+    filename = imgurl.split("/")[-1]
+    with open( filename, mode="wb" ) as f:
+        f.write(imgdata.content)
