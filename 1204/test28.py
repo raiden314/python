0764336f496163107593bc66649fa1d4b0ca5f18
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-12-04 10:53:26 +0900

    エラーとなるものの例を記載しました

--- 1204/test28.py
+++ 1204/test28.py
@@ -5,7 +5,8 @@ url = "https://www.ymori.com/books/python2nen/test2.html"
 
 html = requests.get(url)
 soup = BeautifulSoup(html.content, "html.parser")
-chap2 = soup.find(id="chap2")
+                 #classは予約語だから使えない
+chap2 = soup.find(class="chap2")
 li = chap2.findAll("li")
 for ele in li:
     print(ele.text)
