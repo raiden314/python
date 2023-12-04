daba4cacbabe4f2f03cdc243fc32bf22258517ab
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-12-04 10:40:34 +0900

    htmlの一部の要素を取り出しました

--- 1204/test27.py
+++ 1204/test27.py
@@ -5,4 +5,8 @@ url = "https://www.ymori.com/books/python2nen/test1.html"
 
 html = requests.get(url)
 soup = BeautifulSoup(html.content, "html.parser")
-print(soup)
+# print(soup)
+
+print( soup.find("title") )
+print( soup.find("h2") )
+print( soup.find("h1") )
