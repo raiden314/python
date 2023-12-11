0686762dde2f1534f3137a0b4652d8caf9910b4b
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-12-11 10:43:41 +0900

    ボタンを押すと、おみくじを引ける機能を実装しました。

--- 1211/dialog.py
+++ 1211/dialog.py
@@ -1,11 +1,16 @@
 import tkinter as tk
+import random
+def dispLabel():
+    lst = ["大吉","中吉","小吉","吉","凶","大凶"]
+    i = random.randint(0, len(lst)-1)
+    lbl.configure(text=lst[i])
 
 root = tk.Tk()
 #横200縦100
 root.geometry("200x100")
 
 lbl = tk.Label(text="LABEL")
-btb = tk.Button(text="PUSH")
+btb = tk.Button(text="PUSH",command=dispLabel)
 #ラベルを設置
 lbl.pack()
 btb.pack()
