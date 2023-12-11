760e303e4d0348c290ebe4118fbfcc93df923ce0
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-12-11 11:03:35 +0900

    ファイルを開く関数を実装しました。

--- 1211/selimage.py
+++ 1211/selimage.py
@@ -1,8 +1,13 @@
 import tkinter as tk
+import tkinter.filedialog as fd
+
+def openFile():
+    path = fd.askopenfilename()
+    pass
 
 root = tk.Tk()
 root.geometry("400x350")
 
-btn = tk.Button(text="ファイルを開く")
+btn = tk.Button(text="ファイルを開く", command=openFile)
 btn.pack()
 tk.mainloop()
