bfef0e6f6369ddbd6aad2d5ea65fb0f2fd65e52a
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-12-11 11:55:15 +0900

    写真を開けるように実装しました。

--- 1211/selimage.py
+++ 1211/selimage.py
@@ -1,10 +1,20 @@
 import tkinter as tk
 import tkinter.filedialog as fd
+import PIL.Image
+import PIL.ImageTk
+
+def dispPhoto( path ):
+    newImg = PIL.Image.open( path )
+    #デスクトップアプリ用に変換
+    imgData = PIL.ImageTk.PhotoImage( newImg )
+    imgLbl.configure(image=imgData)
+    #imageプロパティに設定
+    imgLbl.image = imgData
 
 def openFile():
     path = fd.askopenfilename()
     if path:
-        print(path)
+        dispPhoto( path )
 
 root = tk.Tk()
 root.geometry("400x350")
