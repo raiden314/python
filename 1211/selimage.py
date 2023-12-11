dcf97406a4c4f609f97023400efe6802b8e55409
raiden314 <112748994+raiden314@users.noreply.github.com>
2023-12-11 11:05:25 +0900

    画像表示領域を確保しました。

--- 1211/selimage.py
+++ 1211/selimage.py
@@ -3,11 +3,13 @@ import tkinter.filedialog as fd
 
 def openFile():
     path = fd.askopenfilename()
-    pass
 
 root = tk.Tk()
 root.geometry("400x350")
 
 btn = tk.Button(text="ファイルを開く", command=openFile)
+imgLbl = tk.Label()
+
 btn.pack()
+imgLbl.pack()
 tk.mainloop()
