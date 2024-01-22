b721bcaecd4f3dbc63de2c047a73f353def7ed58
raiden314 <112748994+raiden314@users.noreply.github.com>
2024-01-22 12:32:29 +0900

    リスト形式で画像ファイルを表示しました

--- 0122/digitsimg.py
+++ 0122/digitsimg.py
@@ -1,11 +1,13 @@
 from matplotlib import pyplot as plt
 from PIL import Image
+import numpy
 
 path = "0122/2.png"
 imgfile = Image.open(path).convert("L")
-imgfile = imgfile.resize((8,8))
+imgfile = imgfile.resize((8,8), Image.Resampling.LANCZOS)
 
-data = imgfile
+data = numpy.asarray(imgfile,dtype=float)
+print(data)
 
 
 plt.imshow(data, cmap="grey")
