2afab5ee12406231af13fd0a651544be3d886d3f
raiden314 <112748994+raiden314@users.noreply.github.com>
2024-01-22 12:51:40 +0900

    datasetsのサンプルデータを学習させて、作った手書き文字画像を予想させる

--- 0122/predict_digit.py
+++ 0122/predict_digit.py
@@ -1,15 +1,29 @@
+from sklearn import datasets, svm
 from matplotlib import pyplot as plt
 from PIL import Image
 import numpy
 
+digits = datasets.load_digits()
+
+data_train = digits.data
+label_train = digits.target
+
+clf = svm.SVC(gamma=0.001)
+clf.fit(data_train, label_train)
+
+
+
 path = "0122/2.png"
 imgfile = Image.open(path).convert("L")
-#行列に変換
 imgfile = imgfile.resize((8,8), Image.Resampling.LANCZOS)
 data = numpy.asarray(imgfile,dtype=float)
 data = 16 - numpy.floor(17 * data / 256)
-print(data)
-
 
 plt.imshow(data, cmap="Greys")
 plt.show()
+
+data = data.flatten()
+print(data)
+
+n = clf.predict([data])
+print("予測数字＝",n)
