8a220d90fcc8b32c8f54f18fd415107e9dff9685
raiden314 <112748994+raiden314@users.noreply.github.com>
2024-01-22 12:16:10 +0900

    手書き文字の画像を表示

---
+++ 0122/digitsimg.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+import PIL.Image
+
+path = "0122/2.png"
+imgfile = PIL.Image.open(path).convert("L")
+
+data = imgfile
+
+
+plt.imshow(data, cmap="Greys")
+plt.show()
