dc9c8b6274c983dd00d1ba3117517b9c184b3cf6
raiden314 <112748994+raiden314@users.noreply.github.com>
2024-01-22 11:06:11 +0900

    一つのモジュールから、複数の関数をインポートする場合は、fromにした方がいい。

---
+++ 0122/handwriting.py
@@ -0,0 +1,14 @@
+from sklearn import datasets, svm
+from sklearn.model_selection import train_test_split
+
+digits = datasets.load_digits()
+
+#訓練用とテスト用を用意
+data_train, data_test, label_train, label_test = train_test_split(digits.data, digits.target)
+
+print(len(data_train))
+print(len(data_test))
+print(len(label_train))
+print(len(label_test))
+
+clf = svm.SVC()
