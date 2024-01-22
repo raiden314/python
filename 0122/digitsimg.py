42e4cbefadbf3d32ad13aaf960e3d0efb4ae6762
raiden314 <112748994+raiden314@users.noreply.github.com>
2024-01-22 12:35:30 +0900

    リスト形式の数字を見やすく整えました

--- 0122/digitsimg.py
+++ 0122/digitsimg.py
@@ -5,10 +5,10 @@ import numpy
 path = "0122/2.png"
 imgfile = Image.open(path).convert("L")
 imgfile = imgfile.resize((8,8), Image.Resampling.LANCZOS)
-
 data = numpy.asarray(imgfile,dtype=float)
+data = 16 - numpy.floor(17 * data / 256)
 print(data)
 
 
-plt.imshow(data, cmap="grey")
+plt.imshow(data, cmap="Greys")
 plt.show()
