f8daeb38765872b0a66b46b0f31c8464ccc8aae3
raiden314 <112748994+raiden314@users.noreply.github.com>
2024-01-22 11:21:35 +0900

    訓練用のデータを学習させました

--- 0122/handwriting.py
+++ 0122/handwriting.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 
 digits = datasets.load_digits()
 
-#訓練用とテスト用を用意
+#訓練用とテスト用を用意 データと答えを準備
 data_train, data_test, label_train, label_test = train_test_split(digits.data, digits.target)
 
 print(len(data_train))
@@ -12,3 +12,5 @@ print(len(label_train))
 print(len(label_test))
 
 clf = svm.SVC()
+clf.fit(data_train, label_train)
+
