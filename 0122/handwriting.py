f2cfdfa556d06a05b650251abe289abe576e9766
raiden314 <112748994+raiden314@users.noreply.github.com>
2024-01-22 12:02:30 +0900

    datasetsを使って、手書き文字を学習させました。

--- 0122/handwriting.py
+++ 0122/handwriting.py
@@ -14,3 +14,7 @@ print(len(label_test))
 clf = svm.SVC()
 clf.fit(data_train, label_train)
 
+predict = clf.predict( data_test )
+#学習結果が正しいか、答え合わせする 左が導き出した答え、右が正しい答え
+result = list(predict == label_test).count(True) / len(label_test)
+print("正解率＝",result)
